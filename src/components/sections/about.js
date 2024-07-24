8fa830adb1902a26e1938f74efc2819c44588e6d
omair2200 <chatbot.kfas@gmail.com>
2024-07-24 03:07:50 +0300

    resume1

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,14 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'Python',
+    'Manual Testing',
+    'Data Analysis & Preprocessing',
+    'Database Management',
+    'DevOps',
+    'NLP',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +142,21 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Omair Ansari and I enjoy creating data-driven solutions. My interest
+              in software development started back in my college days when I decided to dive into
+              programming and data science.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, and I’ve had the privilege of working at Tivo (Xperi) as a QA
+              Tester and Software Developer, and at Pivony as a Data Scientist. My main focus these
+              days is on building efficient, scalable solutions using Python and various other
+              technologies.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I'm currently on the lookout for a Python Developer position where I can leverage my
+              skills and expertise to solve complex challenges and drive innovation.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,20 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Omair Ansari.</h2>;
+  const three = <h3 className="big-heading">I build things with Python.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a Python Developer specializing in creating and optimizing data-driven solutions.
+        Currently, I’m looking for opportunities to leverage my skills and expertise to solve
+        complex challenges and drive innovation.
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="mailto:ansari.omair10@gmail.com">
+      Get in touch
     </a>
   );
 
