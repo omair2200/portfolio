2a6a31668e204839c88801f2f0803eff719e0ec7
omair22 <ansari.omair10@gmail.com>
2025-08-08 01:36:33 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-db3c747d8da126cb6ec7.js"
+    "url": "webpack-runtime-444f8d09738e7eae1087.js"
   },
   {
     "url": "framework-71f6327df07cb994cfb4.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4d5dd9bfc3479c5b62fa5b51e75f6323"
+    "revision": "a3ec4ed5b692ecc157c878c46b4c685c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6f061b09cc63f0400406.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "92d11953290ca097b49fe92ebb3dd7a2"
+    "revision": "3c9dbf240591c6511bc1f3e9412fb904"
   },
   {
     "url": "manifest.webmanifest",
